519a1fd0bfe88490a96ef7b136c7761b588e96f9
StanleyGunz <stanislavkinsler@gmail.com>
2023-05-24 12:57:46 +0700

    Lesson_Two_HomeWork Commit#2: Added method for checking NEGATIVE MARK of fraction

--- Lesson_Two/SumFraction.py
+++ Lesson_Two/SumFraction.py
@@ -14,39 +14,48 @@ def userInput():  # Ввод
 		return userInput()
 
 
-def sumFraction(a, b):              # Метод сложения дробей
+def sumFraction(a, b, aMark, bMark):  # Метод сложения дробей
 	aNumerator = int(a[len(a) - 3])
 	bNumerator = int(b[len(b) - 3])
 	aDenominator = int(a[len(a) - 1])
 	bDenominator = int(b[len(b) - 1])
-	c = f'{aNumerator * bDenominator + bNumerator * aDenominator}/{aDenominator * bDenominator}'
+	c = f'{aMark*(aNumerator * bDenominator) + bMark*(bNumerator * aDenominator)}/{aDenominator * bDenominator}'
 	return c
 
 
-def multiplyFractions(a, b):        # Метод умножения дробей
+def multiplyFractions(a, b, aMark, bMark):  # Метод умножения дробей
 	aNumerator = int(a[len(a) - 3])
 	bNumerator = int(b[len(b) - 3])
 	aDenominator = int(a[len(a) - 1])
 	bDenominator = int(b[len(b) - 1])
-	c = f'{aNumerator * bNumerator}/{aDenominator * bDenominator}'
+	c = f'{aMark*bMark*(aNumerator * bNumerator)}/{aDenominator * bDenominator}'
 	return c
 
 
-def chooseAction(a, b):             # Выбор действия
+def chooseAction(a, b):  # Выбор действия
 	choice = input("Choose action with fractions:\n"
 	               "1. Summ\n"
 	               "2. Multiply\n")
-
+	aMark = checkNegative(a)
+	bMark = checkNegative(b)
 	match (choice):
 		case "1":
-			return sumFraction(a, b)
+			return sumFraction(a, b, aMark, bMark)
 		case "2":
-			return multiplyFractions(a, b)
+			return multiplyFractions(a, b, aMark, bMark)
 
 
-def mainMethod():                   # Главный метод
+def checkNegative(fraction):
+	if (fraction[0] == "-"):
+		return -1
+	else:
+		return 1
+
+
+def mainMethod():  # Главный метод
 	a = userInput()
 	b = userInput()
+
 	c = chooseAction(a, b)
 	print(f'Your answer is: {c}')
 
